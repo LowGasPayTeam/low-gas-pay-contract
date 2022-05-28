3c12f0347b219f2555c23c77d56f27fae0541b1e
Wiger <wiger@mail.ustc.edu.cn>
2022-05-28 16:47:56 +0800

    Create taskReceiver.py

---
+++ script/task/taskReceiver.py
@@ -0,0 +1,22 @@
+# 获取所有任务
+def getAllTasks():
+    # 任务列表
+    transList = []
+    # 测试循环
+    for i in range (10):
+        # 测试添加
+        transList.append({'from': 'fm'+str(i), 'to': 'to'+str(i), 'token': 'tk'+str(i), 'amount': i, 'transGasFeeMax': 30, 'transGasLimitMax':350000})
+    # 任务总表
+    taskList = {}
+    # 订单编号
+    taskList['orderId'] = '10086'
+    # 订单任务
+    taskList['transaction'] = transList
+
+    # 返回任务
+    return taskList
+
+# 显示结果
+print(getAllTasks())
+
+# 任务 gas
